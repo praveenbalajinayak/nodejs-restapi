c7d24e7edfcccc2f07bebb6c48ea92fe6198e567
Praveen <praveennayaktruminds@gmail.com>
2023-04-04 17:37:14 +0530

    'nodejs'

---
+++ models/index.js
@@ -0,0 +1,27 @@
+
+//const { Sequelize, Datatypes,Model }  = require('sequelize');
+const { Sequelize, DataTypes, Model } = require('sequelize');
+
+const sequelize = new Sequelize('likhith', 'root', 'PRaveen@123', {
+    host: 'localhost',
+    logging : false,
+    dialect: 'mysql'
+  });
+
+  try {
+     sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+
+  const db = {};
+  db.Sequelize = Sequelize;
+  db.sequelize = sequelize; 
+
+  db.contact =  require('./contact')(sequelize,DataTypes)
+  db.user = require('./user')(sequelize,DataTypes,Model);
+
+  db.sequelize.sync({ force: false });
+
+  module.exports = db;
